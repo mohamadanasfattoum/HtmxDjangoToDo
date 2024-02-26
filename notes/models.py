df9c155cae53b60c56219c7c65af4b39c87833ed
Mohamad anas fattoum <samaanas30@gmail.com>
2024-02-26 23:59:38 +0100

    add class todo

--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Todo(models.Model):
+    note = models.TextField(max_length=3000)
+    done = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.note
+    
