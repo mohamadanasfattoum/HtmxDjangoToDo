b0e1dda935ed943b698331d04747bc2e53870fea
Mohamad anas fattoum <samaanas30@gmail.com>
2024-02-28 22:21:54 +0100

    add def create_todo in views

--- notes/views.py
+++ notes/views.py
@@ -5,6 +5,13 @@ from .models import Todo
 
 def todo_list(request):
     data = Todo.objects.all()
-    return render(request,'list.html',{'todos':data})
+    return render(request,'main.html',{'todos':data})
 
+
+def create_todo(requste):
+    title = requste.POST['title']
+    todo = Todo.objects.create(title=title)
+
+    todos = Todo.objects.all()
+    return render(request,'list.html',{'todos':todos})
 
